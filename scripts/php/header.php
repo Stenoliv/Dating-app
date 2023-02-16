e5148518a1ee5520cf032e2d355f9b7b0905ac1f
Ranzz02 <rasmus.raiha@hotmail.com>
2023-02-16 13:01:57 +0200

    flytta index

--- scripts/php/header.php
+++ scripts/php/header.php
@@ -1,15 +1,15 @@
 <header>
     <!-- Logo och meny i headern -->
-    <img src="./media/logo.svg" alt="Website logo" />
+    <img src="../media/logo.svg" alt="Website logo" />
     <div id="logo">DenApp</div>
 
     <nav>
         <!-- Huvudmenyn -->
         <ul>
-            <li><a href="../home/">Home</a></li>
-            <li><a href="../projekt1/">Projekt 1</a></li>
-            <li><a href="../projekt2/">Projekt 2</a></li>
-            <li><a href="../rapport/">Rapport</a></li>
+            <li><a href="./home.php">Home</a></li>
+            <li><a href="./projekt1/">Projekt 1</a></li>
+            <li><a href="./projekt2/">Projekt 2</a></li>
+            <li><a href="./rapport/">Rapport</a></li>
             <?php
             // Hälsa på återkommande användare
             if (isset($_SESSION['username'])) {
