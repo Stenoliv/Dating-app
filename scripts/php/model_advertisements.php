925469d70f9b46a578321c2f43d8e1c809d50884
Stenoliv <stenbaco@arcada.fi>
2023-03-01 17:14:27 +0200

    Början på filtrering samt fortsättning till annons display

--- scripts/php/model_advertisements.php
+++ scripts/php/model_advertisements.php
@@ -1,14 +1,25 @@
 <?php
-
-$sql = "SELECT * FROM profiles LIMIT 5" ;
-$stmt = $conn->query($sql);
+$id=0;
+if(isset($_COOKIE['last_viewd']))
+{
+    $id = $_COOKIE['last_viewd'];
+}
+$sql = "SELECT * FROM profiles WHERE id > ? ORDER BY id LIMIT 5" ;
+$stmt = $conn->prepare($sql);
+$stmt-> execute([$id]);
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-if($_SESSION['username']!="")
+if($stmt-> rowCount()== 0)
+{
+    setcookie('last_viewd','',-1);
+    header("Location:./index.php");
+}
+setcookie('last_viewd',$result[array_key_last($result)]['id'],time()+60*60*24*720);
+if(isset($_SESSION['username']))
 {
     foreach($result as $value)
     {
         print("<div class = 'ads'> <p>".$value['username']."</p>"."<p>".$value['first_name']."</p>"."<p>".$value['last_name']."</p>".
-        "<p>".$value['bio']."</p>"."<p>".$value['salary']."</p>"."<p>".$value['email']."</p>".
+        "<p>".$value['bio']."</p>"."<p>".$value['salary']."</p>"."<p>".$value['email']."</p>"."<p>".$value['zipcode']."</p>".
         "<p>".$value['likes']."</p>");
 
         if($value['gender']==1) $gender = "Male"; 
