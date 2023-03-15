99741a06bb8db6d22d3f329b3d2b871eb248226c
Ranzz02 <rasmus.raiha@hotmail.com>
2023-03-15 22:25:12 +0200

    Added rasmus rapport

--- pages/rapportrasmus.php
+++ pages/rapportrasmus.php
@@ -15,25 +15,37 @@
         <?php include "../scripts/php/header.php" ?>
         <section>
 
-        <article class="rapport">
+            <article class="rapport">
                 <h1 class="raphead1">Rasmus Räihäs rapport</h1>
                 <h2 class="raphead2">Inlärningsprocessen samt hur uppgiften gått</h2>
                 <p class="bröd">
-
+                    Projekt 2 kändes mera givande än projekt 1. Mera riktigt utnyttjande av php språket.
+                    Jag hade inga riktiga svårigheter med Inlärningsprocessen av php det är inte ett så förfärligt svårt språk så att säga.
+                    <br>
+                    <br>
+                    Vi jobba mycket tillsammans löste problem som vi stötte på och på så viss fick båda jobba egentligen på allt tillsammans.
+                    Uppgiften i sig var ju helt underhållande och ett helt bra exempel på en dynamisk websida.
+                    <br>
+                    <br>
+                    Hade kanske mest svårigheter med uppgift 9 då ja försökte köra en lite mera avancerad chat funktionalitet så jag ville spara in meddelandena in i en json array istället för varge meddelande för sig.
+                    Som kanske i det långa inte är det rätta alternativet men jag fick det och funka som jag ville ha det i slutändan.
                 </p>
                 <h3 class="raphead3">Allmän feedback om kursen</h3>
                 <p class="bröd">
-
+                    Super kiva kurs en än gång, roliga uppgifter och du har en allmänt bra takt på inlärningen. Kanske en kommentar att sådana där behändiga grejer som att
+                    splitra if satser i php skulle ha kunnat komma upp tidigare under kursen istället för under sista lektionen,
+                    men annors varit super roligt att jobba med sql och php.
+                    Själv satte jag kanske en 36h på projekt 2 enligt clockify men jag var ganska dålig på att alltid komma ihåg att sätta igång den :D.
                 </p>
-        </article>
+            </article>
 
         </section>
 
         <!-- Footern innehåller t.ex. somelänkar och kontaktuppg -->
-        <?php include "../scripts/php/footer.php"?>
+        <?php include "../scripts/php/footer.php" ?>
 
     </div>
-    
+
 </body>
 
 </html>
